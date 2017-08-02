65a8a78206921864c7f4556a3d8f27c4191240a2
Natalia Larrieux <natalia.larrieux@modelit.xyz>
2017-08-02 20:52:13 -0300

    Add everything

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
+import { AppRoutingModule }     from './app-routing.module';
+import { HttpModule }    from '@angular/http';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppComponent }  from './app.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroSearchComponent } from './hero-search.component';
+
+import { HeroService } from './hero.service';
+
+@NgModule({
+  imports: [ 
+    BrowserModule, 
+    FormsModule ,
+    AppRoutingModule, 
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
+  ],
+  declarations: [ 
+    AppComponent, 
+    HeroesComponent, 
+    HeroDetailComponent, 
+    DashboardComponent, 
+    HeroSearchComponent
+  ],
+  providers: [ HeroService ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
